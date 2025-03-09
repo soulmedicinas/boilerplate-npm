8014c769998f25e066ec9860beafb9641b7ca96d
analytic nutrition <tiramenali@gmail.com>
2025-03-09 16:21:18 -0700

    add: .env file

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -5,7 +7,9 @@ const port = process.env.PORT || 3000;
 console.log("Hello World");
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+  res.json({"message": "HELLO JSON"});
+  process.env.MESSAGE_STYLE;
+  process.env.MESSAGE_STYLE=uppercase;
 });
 
 app.use("/public", express.static(__dirname + "/public"));
